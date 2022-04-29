ca5ea0f9b049904a9beb2a7f03d40b88a44f5a40
TahaJamil37 <tahamian37@gmail.com>
2022-04-29 12:48:34 +0500

    Apis integration

--- src/views/pages/blog/BlogSidebar.js
+++ src/views/pages/blog/BlogSidebar.js
@@ -7,7 +7,7 @@ import { mapcontainer } from '../store/mapcontainer'
 import { farmidcommunicator } from '../store/farmidcommunicator'
 import axios from 'axios'
 import * as Icon from 'react-feather'
-import { useLayoutEffect,useState } from 'react'
+import { useLayoutEffect, useState, useEffect } from 'react'
 import { Msidn } from '../store/msidn'
 // ** Custom Components
 import Avatar from '@components/avatar'
@@ -17,20 +17,75 @@ import { InputGroup, Input, InputGroupText, Card, CardHeader, CardTitle, CardBod
 
 const BlogSidebar = (prop) => {
   // ** States
-  const [farmids,setFarmids]=useState()
+  const [farmids, setFarmids] = useState()
+  const [overviewData, setOverviewData] = useState([])
+  const [farmerList, setfarmerList] = useState([])
 
-  const CategoryColorsArr = {
-    Quote: 'light-info',
-    Fashion: 'light-primary',
-    Gaming: 'light-danger',
-    Video: 'light-warning',
-    Food: 'light-success'
-  }
   const [map, setMap] = useState(mapcontainer.getState())
+
+  useEffect(async () => {
+    let res = await axios.get("https://gistest.bkk.ag/Partner_stats/hbl");
+    setOverviewData(res.data[0])
+    getFarmersList()
+
+  }, [])
+
+  const getFarmersList = async () => {
+    let res = await axios.get("https://gistest.bkk.ag/HBL_overview/hbl");
+    setfarmerList(res.data)
+  }
+
+
+
+  const renderTransactions = (farmerList) => {
+    return farmerList.map(item => {
+      return (
+        <Card className='card-transaction mt-1' >
+          <CardAction title={item?.farmer_name} actions='collapse'>
+
+            <CardBody>
+              <div className='meetup-header d-flex align-items-center'>
+
+                <div className='my-auto'>
+                  <h6>  Crops</h6>
+
+                  <CardText className='mb-0  text-success'>{item.crops}</CardText>
+                </div>
+              </div>
+              <div className='d-flex mt-2'>
+                <Avatar color='light-primary' className='rounded me-1' icon={<Icon.Phone size={18} />} />
+                <div>
+                  <h6 className='mb-0'>MSISDN</h6>
+                  <small >{item.msisdn}</small>
+                </div>
+              </div>
+              <div className='d-flex mt-1'>
+                <Avatar color='light-primary' className='rounded me-1' icon={<Icon.Check size={18} />} />
+                <div>
+                  <h6 className='mb-0'>Fields</h6>
+                  <small >{overviewData.fields}</small>
+                </div>
+              </div>
+              <div className='d-flex mt-2'>
+                <Avatar color='light-primary' className='rounded me-1' icon={<Icon.MapPin size={18} />} />
+                <div>
+                  <h6 className='mb-0'>Total Area</h6>
+                  <small>{overviewData.total_area}</small>
+                </div>
+              </div>
+
+
+
+            </CardBody>
+          </CardAction>
+        </Card>
+      )
+    })
+  }
   useLayoutEffect(() => {
     mapcontainer.subscribe(() => {
       setMap(mapcontainer.getState())
-  
+
 
     })
   }, [])
@@ -39,101 +94,122 @@ const BlogSidebar = (prop) => {
       <div className='sidebar'>
         <div className='blog-sidebar right-sidebar my-1 my-lg-0'>
           <div className='right-sidebar-content'>
+
+            <Card className='card-transaction' >
+              <CardAction title='HBL Overview' actions='collapse'>
+
+                <CardBody>
+                  <div className='meetup-header d-flex align-items-center'>
+
+                    <div className='my-auto'>
+                      <h6>  Locations</h6>
+
+                      <CardText className='mb-0  text-success'>{overviewData.locations}</CardText>
+                    </div>
+                  </div>
+                  <div className='d-flex mt-1'>
+                    <Avatar color='light-primary' className='rounded me-1' icon={<Icon.Check size={18} />} />
+                    <div>
+                      <h6 className='mb-0'>Fields</h6>
+                      <small >{overviewData.fields}</small>
+                    </div>
+                  </div>
+                  <div className='d-flex mt-2'>
+                    <Avatar color='light-primary' className='rounded me-1' icon={<Icon.MapPin size={18} />} />
+                    <div>
+                      <h6 className='mb-0'>Total Area</h6>
+                      <small>{overviewData.total_area}</small>
+                    </div>
+                  </div>
+                  <div className='d-flex mt-2'>
+                    <Avatar color='light-primary' className='rounded me-1' icon={<Icon.Crop size={18} />} />
+                    <div>
+                      <h6 className='mb-0'>Crops Count</h6>
+                      <small >{overviewData.count_crops}</small>
+                    </div>
+                  </div>
+                  <div className='d-flex mt-2'>
+                    <Avatar color='light-primary' className='rounded me-1' icon={<Icon.MapPin size={18} />} />
+                    <div>
+                      <h6 className='mb-0'>Location Count</h6>
+                      <small >{overviewData.count_locations}</small>
+                    </div>
+                  </div>
+
+                </CardBody>
+              </CardAction>
+            </Card>
             <div className='blog-search'>
               <InputGroup className='input-group-merge'>
-                <Input placeholder='Search here'  onKeyDown={(e)=>{
-                  
-                  console.log('event',e.key)
+                <Input placeholder='Search here' onKeyDown={(e) => {
+
+                  console.log('event', e.key)
                   if (e.key == 'Enter') {
-console.log('enter',e.target.value)
-var msidn=e.target.value
-                     Msidn.dispatch({type:'msidn',msidn:msidn})
-                  //   setMsidn(msidn)
-                  //   console.log(msidn)
+                    console.log('enter', e.target.value)
+                    var msidn = e.target.value
+                    Msidn.dispatch({ type: 'msidn', msidn: msidn })
+                    //   setMsidn(msidn)
+                    //   console.log(msidn)
                     //  msidn.dispatch({type:'msidn',msidn:d.data.features[0]['properties']['field_1']})
-                      axios.get("https://gis.bkk.ag/geoserver/server/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=server%3Ahbl_farms&CQL_FILTER=msisdn='"+msidn+"'&outputFormat=application%2Fjson").then(
-               r=>{
-                console.log('enter',r)
-                  var bbx=r.data.features[0]['geometry']['coordinates'][0][0][0]
-              //     console.log('wmslayer',bbx)
-              //     console.log(map)
-              
-                  map.flyTo([bbx[1],bbx[0]],15)
-                 
-                 
-                r.data.features.map(d=>{
-              //     // console.log(d)
-              map.on('click',e=>{
-                console.log(e.latlng)
-                 var sw = map.options.crs.project(map.getBounds().getSouthWest());
-         var ne = map.options.crs.project(map.getBounds().getNorthEast());
-         var BBOX = sw.x + "," + sw.y + "," + ne.x + "," + ne.y;
-         var WIDTH = map.getSize().x;
-         var HEIGHT = map.getSize().y;
-         var X = Math.trunc(map.layerPointToContainerPoint(e.layerPoint).x);
-         var Y = Math.trunc(map.layerPointToContainerPoint(e.layerPoint).y);
-         var url = "https://gis.bkk.ag/geoserver/server/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetFeatureInfo&LAYERS=server:hbl_farms&CQL_FILTER=msisdn='"+msidn+"'&QUERY_LAYERS=server:hbl_farms&BBOX="+BBOX+"&FEATURE_COUNT=1&HEIGHT="+HEIGHT+"&WIDTH="+WIDTH+"&INFO_FORMAT=application%2Fjson&TILED=false&CRS=EPSG%3A3857&I="+X+"&J="+Y;
-         axios.get(url).then(
-             d=>
-        {
-              console.log('idvalue', d.data.features[0]['properties']['farm_crop_id'])
-              d.data.features[0]['properties']['farm_crop_id'] ?   farmidcommunicator.dispatch({type:'search',id:d.data.features[0]['properties']['farm_crop_id']}) :  <></>
-               setFarmids(d.data.features[0]['properties']['farm_crop_id'])
-             
-             }
-            )
-        // var bbxs=r.data.features[0]['properties']['cordinate'].split(',')
-        //  var lats=bbxs[0]
-        //  var lngs=bbxs[1]
-        
-           
-        //    lat.dispatch({type:'lat',lat:lats})
-        //    lngt.dispatch({type:'lng',lng:lngs})
-        
-             })
-                 
-               })
-               })
-                     return true;
+                    axios.get("https://gis.bkk.ag/geoserver/server/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=server%3Ahbl_farms&CQL_FILTER=msisdn='" + msidn + "'&outputFormat=application%2Fjson").then(
+                      r => {
+                        console.log('enter', r)
+                        var bbx = r.data.features[0]['geometry']['coordinates'][0][0][0]
+                        //     console.log('wmslayer',bbx)
+                        //     console.log(map)
+
+                        map.flyTo([bbx[1], bbx[0]], 15)
+
+
+                        r.data.features.map(d => {
+                          //     // console.log(d)
+                          map.on('click', e => {
+                            console.log(e.latlng)
+                            var sw = map.options.crs.project(map.getBounds().getSouthWest());
+                            var ne = map.options.crs.project(map.getBounds().getNorthEast());
+                            var BBOX = sw.x + "," + sw.y + "," + ne.x + "," + ne.y;
+                            var WIDTH = map.getSize().x;
+                            var HEIGHT = map.getSize().y;
+                            var X = Math.trunc(map.layerPointToContainerPoint(e.layerPoint).x);
+                            var Y = Math.trunc(map.layerPointToContainerPoint(e.layerPoint).y);
+                            var url = "https://gis.bkk.ag/geoserver/server/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetFeatureInfo&LAYERS=server:hbl_farms&CQL_FILTER=msisdn='" + msidn + "'&QUERY_LAYERS=server:hbl_farms&BBOX=" + BBOX + "&FEATURE_COUNT=1&HEIGHT=" + HEIGHT + "&WIDTH=" + WIDTH + "&INFO_FORMAT=application%2Fjson&TILED=false&CRS=EPSG%3A3857&I=" + X + "&J=" + Y;
+                            axios.get(url).then(
+                              d => {
+                                console.log('idvalue', d.data.features[0]['properties']['farm_crop_id'])
+                                d.data.features[0]['properties']['farm_crop_id'] ? farmidcommunicator.dispatch({ type: 'search', id: d.data.features[0]['properties']['farm_crop_id'] }) : <></>
+                                setFarmids(d.data.features[0]['properties']['farm_crop_id'])
+
+                              }
+                            )
+                            // var bbxs=r.data.features[0]['properties']['cordinate'].split(',')
+                            //  var lats=bbxs[0]
+                            //  var lngs=bbxs[1]
+
+
+                            //    lat.dispatch({type:'lat',lat:lats})
+                            //    lngt.dispatch({type:'lng',lng:lngs})
+
+                          })
+
+                        })
+                      })
+                    return true;
                   } else {
-                     return false;
+                    return false;
                   }
-                }}/>
+                }} />
+
                 <InputGroupText>
                   <Icon.Search size={14} />
                 </InputGroupText>
               </InputGroup>
             </div>
-            <Card className='business-card mt-2'>
-              <CardHeader className='pb-1'>
-                <CardTitle tag='h4'>Fields</CardTitle>
-                <Icon.Edit size={18} className='cursor-pointer' />
-              </CardHeader>
-              <CardBody>
-                <CardText>US Demo Field</CardText>
-                <span>10.5 ha</span>
-                <br />
-                <span >39.5119° N 122.0711° W</span>
 
 
-              </CardBody>
-            </Card>
-            <Card className='business-card'>
-              <CardHeader className='pb-1'>
-                <CardTitle >Crop rotate <Icon.Info size={12} className='cursor-pointer' /></CardTitle>
-                <p className='mt-1'>Show more</p>
-              </CardHeader>
 
-            </Card>
-            <CardAction title='Current weather' actions='collapse'>
-              <CardBody className='pt-0'>
 
-                <CardText>
-                  Apr 27, 2022, 00:00 - 01:00
-                </CardText>
 
-              </CardBody>
-            </CardAction>
+            <div style={{ height: "250px", overflowY: "scroll" }} className="mt-1">{renderTransactions(farmerList)}</div>
 
           </div>
         </div>
